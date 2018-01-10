5b0fd9ac16ea1a2567f9c62526c91b96d6888e29
ss7424Refar <812647742@qq.com>
2018-01-10 16:07:07 +0800

    2018/01/10: 4. Control Flow Tools from python website/ Finish Learning

---
+++ 4.Control Flow Tools/Test_Documents.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+def my_function():
+    """Do nothing, but document it.
+                    No, really, it doesn't do anything.
+    """
+# 没有括号
+print my_function.__doc__
--- 4.Control Flow Tools/Test_defUnpacking.py
+++ 4.Control Flow Tools/Test_defUnpacking.py
@@ -1,2 +1,9 @@
 args = [1, 6, 7]
 print list(range(*args))
+
+def parrot(voltage, state='stuff', action='voom'):
+    print "parrot", voltage, state, action
+
+words = {"voltage": "hello", "state": "save", "action": "voo"}
+parrot(words)
+parrot(**words)
